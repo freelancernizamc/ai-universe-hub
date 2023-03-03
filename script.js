f546686ecae001cebef70238104b7c26dfd2a223
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-03-03 11:22:15 +0600

    all data loaded

--- script.js
+++ script.js
@@ -6,17 +6,46 @@ Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
 Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
 */
 
-function dataLoad(){
-    fetch(' https://openapi.programming-hero.com/api/ai/tools')
-    .then(response => response.json())
-    .then(data => displayData(data))
-}
-
-const displayData = universe =>{
-    // const universeContainer = document.getElementById("universe");
-    console.log(universe);
-}
 
 
+
+const dataLoad = async() =>{
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayData(data.data.tools);
+};
+
+const displayData = tools => {
+  
+  const universeContainer = document.getElementById('universe-hug');
+  universeContainer.innerHTML = "";
+ 
+  tools.forEach (tool =>{
+      const toolDiv = document.createElement('div');
+      toolDiv.classList.add('col');
+      toolDiv.innerHTML = `
+          <div class="card">
+            <img src="${tool.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5>Feature</h5>
+              <ol>
+              <li>Natural language processing</li>
+              <li>Contextual understanding</li>
+              <li>Text generation </li>
+              </ol>
+              <hr>
+              
+              <h5 class="card-text">${tool.name}</h5>
+              <span class="date"><i class="fa fa-calendar"></i> 11/01/2022</span>
+
+            </div>
+          </div>
+      `;
+      universeContainer.appendChild(toolDiv);
+  });
+};
 
 dataLoad();
+
+
