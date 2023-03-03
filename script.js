ecc799a66007789a235e2ad822c2fed6ada884a2
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-03-04 01:04:19 +0600

    some data show in modal

--- script.js
+++ script.js
@@ -34,22 +34,17 @@ const displayData = (tools) => {
             <img src="${tool.image}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5>Feature</h5>
-              <ol>
-              <li>Natural language processing</li>
-              <li>Contextual understanding</li>
-              <li>Text generation </li>
-              </ol>
+            <ol><li>${tool.features}<li></ol>
+              
               <hr>
               
               <h5 class="card-text">${tool.name}</h5>
               <div class="d-flex justify-content-between">
-              <span class="date"><i class="fa fa-calendar"></i> 11/01/2022</span>&emsp;
-              <span onclick="displayDetails(${tool.id})" class="text-danger fw-bold" data-bs-toggle="modal"
+              <span class="date"><i class="fa fa-calendar"></i>  ${tool.published_in}</span>&emsp;
+              <span onclick="displayDetails('${tool}')" class="text-danger fw-bold" data-bs-toggle="modal"
               data-bs-target="#myModal"> &#x2192;</span>
 
-
-
-              </div>
+            </div>
 
 
             </div>
@@ -57,11 +52,8 @@ const displayData = (tools) => {
       `;
       universeContainer.appendChild(toolDiv);
   });
+  
 };
-
-
-
-
 
 
 
@@ -76,36 +68,48 @@ const showAllData = () =>{
 });
 }
 
-const loadDetails = async () =>{
-  const url =`https://openapi.programming-hero.com/api/ai/tool/01`;
+dataLoad();
+
+const loadDetails = async (tool) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/01`;
   const res = await fetch(url);
   const data = await res.json();
   displayDetails(data.data.tools);
-}
-
+};
 
 const displayDetails = (tools) => {
   const modalTitle = document.getElementById('myModalLabel2');
-  const tooldetails = document.getElementById('details');
+  const cardDetails = document.getElementById('details');
   let toolHTML = '';
-  
-  tools.forEach(tool => {
-    toolHTML += `
-      <div class="row g-0">
-        <div class="col-md-4" id="img">
-          <img src="${data.tool.image_link}" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5>${tool.tool_name}</h5>
-            <p>${tool.description}</p>
+  console.log(tools);
+  toolHTML += `
+    <div class="row g-0 modal-xl">
+      <div class="col-md-4" id="img">
+        <img src="${tools.image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5>${tools.description}</h5>
+          <div class="d-flex">
+          <div class="text-success text-center w-25 shadow"><p>Free of Cost/Basic</p></div>
+          <div class="text-warning text-center w-25 shadow"><p>Free Of Cost/Pro</p></div>
+          <div class="text-danger text-center w-25 shadow"><p>Free of Cost /Enterprise</p></div>
           </div>
+          <div class="d-flex">
+          <div>
+          <h5>Feature</h5>
+          <ul><li>${tools.features}</li></ul>
+          </div>
+          <div>
+          <h5>Integrations</h5>
+          <ul><li>${tools.integrations}</li></ul>
+        </div>
         </div>
       </div>
-    `;
-  });
+    </div>
+  `;
 
-  tooldetails.innerHTML = toolHTML;
+  cardDetails.innerHTML = toolHTML;
 
   // Show the modal
   const myModal = new bootstrap.Modal(document.getElementById('myModal'));
@@ -113,4 +117,8 @@ const displayDetails = (tools) => {
 };
 
 
-dataLoad();
+
+
+
+
+loadDetails();
